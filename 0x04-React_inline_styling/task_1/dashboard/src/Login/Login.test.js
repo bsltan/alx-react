978747afaaf816648acd7e5d4b6ebdda06a8d571
bsltan <califchichi@yahoo.com>
2023-09-05 19:05:47 +0300

    Task 1

--- 0x04-React_inline_styling/task_1/dashboard/src/Login/Login.test.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/Login/Login.test.js
@@ -1,18 +1,23 @@
+import { shallow } from "enzyme";
 import React from "react";
 import Login from "./Login";
-import { shallow } from "enzyme";
+import { StyleSheetTestUtils } from "aphrodite";
 
-describe('Login tests', () => {
-  it("renders without crashing", () => {
+beforeEach(() => {
+  StyleSheetTestUtils.suppressStyleInjection();
+});
+afterEach(() => {
+  StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
+});
+
+describe("Header", () => {
+  it("should render without crashing", () => {
     const wrapper = shallow(<Login />);
-    expect(wrapper.exists()).toBe(true);
+    expect(wrapper.exists()).toEqual(true);
   });
-  it('renders 2 input tags', () => {
-    const wrapper = shallow(<Login />)
-    expect(wrapper.find('input')).toHaveLength(2);
+  it("should have 2 input tags and 2 label tags", () => {
+    const wrapper = shallow(<Login />);
+    expect(wrapper.find("label")).toHaveLength(2);
+    expect(wrapper.find("input")).toHaveLength(2);
   });
-  it('renders 2 label tags', () => {
-    const wrapper = shallow(<Login />)
-    expect(wrapper.find('label')).toHaveLength(2);
-  })
-})
+});
